91d6dcc5f03d56b7faab8c4e5951cd186ce82552
seemamj <seemamj.94@gmail.com>
2018-09-10 10:51:18 +0530

    first commit

---
+++ restws/src/main/java/com/seema/demo/RestwsApplication.java
@@ -0,0 +1,12 @@
+package com.seema.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestwsApplication.class, args);
+	}
+}
